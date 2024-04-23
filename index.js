a0caf122fd3d548d820d91f484da5e75d492ed31
Pranjal Keshari <kingkeshari82@gmail.com>
2024-04-24 00:47:31 +0530

    JavaScript File

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let compScore =0;
 let userScore=0;
-
+let msg= document.querySelector("#msg");
 let choices= document.querySelectorAll(".choice");
 
 
@@ -22,9 +22,60 @@ const compChoicefun= ()=>{
 }
 
 
+
+
+
+const drawgame=()=>{
+    msg.innerText="Game Draw";
+    msg.style.backgroundColor="blue";
+    console.log("Game Draw");
+}
+
+const showwinner=(userwin)=>{
+    if(userwin)
+    {   msg.innerText="You Win";
+    msg.style.backgroundColor=" rgb(6, 201, 38)";
+    // msg.classList.remove("lose");
+    //     msg.classList.add("won");
+        
+        console.log("You win");
+    }else{
+        msg.innerText="You lose";
+        msg.style.backgroundColor="red";
+        // msg.classList.remove("won");
+        // msg.classList.add("lose");
+        console.log("you lose");
+    }
+}
+                 
+
+
 const playGame=(userChoice)=>{
     let compChoice= compChoicefun();
     console.log("user choice",userChoice);
     console.log("comp choice",compChoice);
+
+    if(userChoice===compChoice)
+    {
+        drawgame();
+    }
+    else 
+    {
+        let userwin= true;
+        if(userChoice==="rock")
+        {
+         userwin = compChoice==="paper"?false:true; 
+        }
+        else if(userChoice==="paper") {
+            userwin=compChoice==="scissor"?false:true;
+        }
+        else{
+            userwin= compChoice==="rock"?false:true;
+        }
+
+        showwinner(userwin);
+    }
+
     
-}
+    
+};
